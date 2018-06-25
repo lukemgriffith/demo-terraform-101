33bb7930a3fd83c05a1af8009d906b283303fa5b
Luke M Griffith <lukemgriffith@gmail.com>
2018-06-25 11:17:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+  # MODIFY this line to look for 1.20.0 or greater
+  version = ">= 1.20.0"
 }
 
 resource "aws_instance" "web" {
